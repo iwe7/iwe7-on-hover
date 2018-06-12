7bfd2797876af00e647b161d9c6f1ebc2bc77561
iwe7 <admin@meepo.com.cn>
2018-06-12 21:04:44 +0800

    fixed export error

--- src/lib/iwe7-on-hover.module.ts
+++ src/lib/iwe7-on-hover.module.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { Iwe7OnHoverComponent } from './iwe7-on-hover.component';
+import { NgOnHoverDirective } from './on-hover';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [Iwe7OnHoverComponent],
-  exports: [Iwe7OnHoverComponent]
+  declarations: [NgOnHoverDirective],
+  exports: [NgOnHoverDirective]
 })
 export class Iwe7OnHoverModule { }
